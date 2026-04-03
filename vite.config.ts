4e03fb8befe7510bf2c770218ae3952c651ceac9
bethfrankosfreelance <bethfrankosfreelance@gmail.com>
2026-04-03 10:56:51 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+// https://bitejs.dev
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: "/Rasmee/",
