c7e20497ed7db9d3d4f10ae8260e2d2738e7cb83
bethfrankosfreelance <bethfrankosfreelance@gmail.com>
2026-04-03 20:16:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Rasmee/",
+  base: "/",
   server: {
     host: "0.0.0.0",
     port: 5000,
